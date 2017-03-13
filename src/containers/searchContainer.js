d8369945bb17f221e72e0b9f50252f7e38d287e9
Dawid Gorczyca <dawid.gorczyca@globallogic.com>
2017-03-13 12:06:04 +0100

    ejected, testing youtube api

---
+++ src/containers/searchContainer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class SearchContainer extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return(
+      <div className="search">
+        
+      </div>
+    )
+  }
+}
+
+SearchContainer.propTypes = {
+  searchParams: React.PropTypes.object
+}
+
+export default SearchContainer
